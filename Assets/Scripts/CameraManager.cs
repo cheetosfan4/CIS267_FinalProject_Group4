af998016d3347e7574567d66aa03efcd47b0d793
cheetosfan4 <146988149+cheetosfan4@users.noreply.github.com>
2025-11-27 10:29:18 -0500

    prefab changes

--- Assets/Scripts/CameraManager.cs
+++ Assets/Scripts/CameraManager.cs
@@ -1,6 +1,11 @@
 using UnityEngine;
 
 public class CameraManager : MonoBehaviour {
+    private SpriteRenderer sr;
+    private void Awake() {
+        sr = GetComponentInChildren<SpriteRenderer>();
+        sr.enabled = true;
+    }
     public void moveToRoom(Vector3 coordinates) {
         this.transform.position = coordinates;
     }
